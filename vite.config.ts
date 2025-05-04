c8b7429c8541cd71578781fff0f760540fe9cdad
iansalandy <ihsaland@gmail.com>
2025-05-04 07:10:24 -0400

    Update paths for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/sudokuCV/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/',
   build: {
     assetsDir: 'assets',
     outDir: 'dist',
@@ -25,7 +19,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     open: true
