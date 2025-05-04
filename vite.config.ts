4daaa4c4bc9252b31b5a040ebc59f5cec9f252cf
iansalandy <ihsaland@gmail.com>
2025-05-04 00:02:34 -0400

    Fix styling and asset loading issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/sudokuCV/',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
